360b40f2d06ad2e1a48c674b0a6f6abdd44f06a7
nam.pd <phamducnamtl2001@gmail.com>
2024-03-23 22:34:27 +0700

    write JWTService to generate, handle, check token, extract claim...

--- src/main/java/dev/nampd/realnote/jwt/JWTService.java
+++ src/main/java/dev/nampd/realnote/jwt/JWTService.java
@@ -1,4 +1,8 @@
 package dev.nampd.realnote.jwt;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface JWTService {
+
 }
--- src/main/java/dev/nampd/realnote/jwt/JWTServiceImpl.java
+++ src/main/java/dev/nampd/realnote/jwt/JWTServiceImpl.java
@@ -1,4 +1,74 @@
 package dev.nampd.realnote.jwt;
 
-public class JWTServiceImpl {
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwts;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JWTServiceImpl implements JWTService {
+
+    //tạo chuỗi token jwt dựa trên userDetails
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //tạo refresh token
+    public String generateRefreshToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder().setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 604800000))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //trích xuất tt chứa trong chuỗi token JWT
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    //trích xuất một claim cụ thể từ chuỗi token JWT
+    //và chuyển đổi nó thành một loại dữ liệu cụ thể
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    //tạo key trong quá trình ký (sign) chuỗi token jwt
+    public Key getSignInKey() {
+        byte[] key = Decoders.BASE64.decode("5d9bcee44a9f0549cf53d86206bf8d1e98c675ce843bab16685e9d1056e64633");
+        return Keys.hmacShaKeyFor(key);
+    }
+
+    //ktra token hết hạn
+    public boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+    //ktra token hợp lệ
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 }
